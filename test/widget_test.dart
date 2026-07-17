97cf163662500cbef47b71ab8f0db3a731c18ebc
valera2872 <barbarov28@protonmail.com>
2026-07-17 22:10:49 +0300

    Provide supervisor controller in widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,18 +5,25 @@ import 'package:supervision_pocket/app/app_controller.dart';
 import 'package:supervision_pocket/core/security/security_store.dart';
 import 'package:supervision_pocket/features/cases/application/case_controller.dart';
 import 'package:supervision_pocket/features/cases/data/case_repository.dart';
+import 'package:supervision_pocket/features/supervisor/application/supervisor_controller.dart';
+import 'package:supervision_pocket/features/supervisor/data/supervisor_repository.dart';
 
 void main() {
   testWidgets('first launch presents the premium product promise', (tester) async {
     final controller = AppController(MemorySecurityStore());
     final caseController = CaseController(MemoryCaseRepository());
+    final supervisorController = SupervisorController(
+      MemorySupervisorRepository(),
+    );
     await controller.initialize();
     await caseController.initialize();
+    await supervisorController.initialize();
 
     await tester.pumpWidget(
       SupervisionPocketApp(
         controller: controller,
         caseController: caseController,
+        supervisorController: supervisorController,
       ),
     );
     await tester.pumpAndSettle();
