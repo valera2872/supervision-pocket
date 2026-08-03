3f6e16f8234ffcaec64cf19252981be598a56758
valera2872 <barbarov28@protonmail.com>
2026-08-03 22:54:36 +0300

    Add Supabase project configuration

---
+++ lib/config/supabase_config.dart
@@ -0,0 +1,10 @@
+class SupabaseConfig {
+  const SupabaseConfig._();
+
+  static const url = 'https://hbnevhhpveyrqgmoient.supabase.co';
+  static const publishableKey =
+      'sb_publishable_KjZYalm_lvhDV98WhRboBA_WlteMEF1';
+
+  static const invitationBaseUrl =
+      'https://valera2872.github.io/supervision-pocket/connect';
+}
